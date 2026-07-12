941a05e91a2ff92057d15dace0a3ca3ea4c7465e
Oleskii <oleksiy.tkachuk.work@gmail.com>
2026-07-12 14:26:53 +0300

    fix styling in order to flake8

--- app/main.py
+++ app/main.py
@@ -1,22 +1,31 @@
 class Animal:
 
-
     alive = []
 
-    def __init__(self, name: str, health: int = 100, hidden: bool = False) -> None:
+    def __init__(
+            self,
+            name: str,
+            health: int = 100,
+            hidden: bool = False
+    ) -> None:
+
         self.name = name
         self.health = health
         self.hidden = hidden
 
         Animal.alive.append(self)
 
-    def __repr__(self):
-        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
+    def __repr__(self) -> str:
+        return (f"{{Name: {self.name}, "
+                f"Health: {self.health}, "
+                f"Hidden: {self.hidden}}}")
+
 
 class Herbivore(Animal):
 
     def hide(self) -> None:
         self.hidden = not self.hidden
+
 
 class Carnivore(Animal):
 
@@ -24,5 +33,5 @@ class Carnivore(Animal):
     def bite(victim: Herbivore) -> None:
         if isinstance(victim, Herbivore) and not victim.hidden:
             victim.health -= 50
-            if victim.health <=0:
+            if victim.health <= 0:
                 Animal.alive.remove(victim)
