d88b311312a0f55ac3ce21f31e59f9967b84170a
Oleskii <oleksiy.tkachuk.work@gmail.com>
2026-07-12 14:19:44 +0300

    implemented parent class animal with child classes Herbivore and Carnivore

--- app/main.py
+++ app/main.py
@@ -1,1 +1,33 @@
-# write your code here
+class Animal:
+
+
+    alive = []
+
+    def __init__(self, name: str, health: int = 100, hidden: bool = False) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+
+        Animal.alive.append(self)
+
+    def __str__(self):
+        return str([
+            f"Name: {animal.name}, Health: {animal.health}, Hidden: {animal.hidden}"
+            for animal in Animal.alive])
+
+    def __repr__(self):
+        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
+
+class Herbivore(Animal):
+
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+class Carnivore(Animal):
+
+    @staticmethod
+    def bite(victim: Herbivore) -> None:
+        if isinstance(victim, Herbivore) and not victim.hidden:
+            victim.health -= 50
+            if victim.health <=0:
+                Animal.alive.remove(victim)
